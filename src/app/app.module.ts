57c6ef2bb0862858f5ab470e5e8f4e43a463e29a
juan gutierrez <juancito_rulo21@hotmail.com>
2023-01-18 21:07:11 -0300

    primer desafio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+
+import { StudentComponent } from './components/student/student.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
+    
+    StudentComponent,
+    ToolbarComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
